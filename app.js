608af2af71ff146c467ab7a5df2e5e79391349f7
ajaymedidhi <ajaymedidhi858@gmail.com>
2024-12-31 21:24:47 +0530

    testing api

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: 'https://contactmanagement7.vercel.app' }));
+app.use(cors({ origin: 'http://localhost:3000'  }));
 app.use(bodyParser.json());
 
 // Routes
